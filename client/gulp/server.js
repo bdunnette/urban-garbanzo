f096633e5be351ba78f7e208dad23cd046c70c7e
Brian Dunnette <dunn0172@umn.edu>
2016-01-29 12:29:09 -0600

    adding gulp-nodemon

--- client/gulp/server.js
+++ client/gulp/server.js
@@ -2,6 +2,7 @@
 
 var path = require('path');
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 var conf = require('./conf');
 
 var browserSync = require('browser-sync');
@@ -15,7 +16,7 @@ function browserSyncInit(baseDir, browser) {
   browser = browser === undefined ? 'default' : browser;
 
   var routes = null;
-  if(baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
+  if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
     routes = {
       '/bower_components': 'bower_components'
     };
@@ -43,21 +44,29 @@ function browserSyncInit(baseDir, browser) {
 }
 
 browserSync.use(browserSyncSpa({
-  selector: '[ng-app]'// Only needed for angular apps
+  selector: '[ng-app]' // Only needed for angular apps
 }));
 
-gulp.task('serve', ['watch'], function () {
-  browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+gulp.task('serve', ['watch'], function() {
+browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
+  nodemon({
+    // the script to run the app
+    script: '../server/server.js',
+    watch: ['../server/', '../server/boot/'],
+    ext: 'js json'
+  }).on('start', function() {
+  }).on('restart', function() {
+  });
 });
 
-gulp.task('serve:dist', ['build'], function () {
+gulp.task('serve:dist', ['build'], function() {
   browserSyncInit(conf.paths.dist);
 });
 
-gulp.task('serve:e2e', ['inject'], function () {
+gulp.task('serve:e2e', ['inject'], function() {
   browserSyncInit([conf.paths.tmp + '/serve', conf.paths.src], []);
 });
 
-gulp.task('serve:e2e-dist', ['build'], function () {
+gulp.task('serve:e2e-dist', ['build'], function() {
   browserSyncInit(conf.paths.dist, []);
 });
